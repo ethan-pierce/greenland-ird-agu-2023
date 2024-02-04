8b2a0b7d461decc228ebc24860aee023431d484b
Ethan Pierce <ethan.pierce@colorado.edu>
2024-02-04 09:57:52 -0700

    Update hydrology prototype

--- components/hydrology/schoof-1d-prototype.py
+++ components/hydrology/schoof-1d-prototype.py
@@ -1,6 +1,10 @@
 import numpy as np
+from scipy.optimize import least_squares
 import jax
 import jax.numpy as jnp
+from jax import config
+config.update("jax_enable_x64", True)
+
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 14})
 
@@ -28,58 +32,29 @@ bed = -4.563492 - 0.07106481 * grid + 0.00004484127 * grid**2 - 5.324074e-9 * gr
 surface = h0 * jnp.sqrt(grid)
 thickness = surface - bed
 base_gradient = jnp.asarray(-np.gradient(rho_i * g * thickness, grid) - rho_w * g * np.gradient(bed, grid))
+discharge = (L0 - grid) * m
 
 # Solver
-def run_one_step(dt, S, psi, N):
-    dSdt = c1 * m * psi + v0 * (1 - S / S0) - c2 * S * N**n
-    S += dSdt * dt
-    Q = c3 * S**a * jnp.abs(psi)**(-1/2) * psi
-    Q.at[-1].set(0.0)
+def solve(psi, Q, S):
+    return c3 * S**a * psi - Q
 
-    dNdt = (m - np.gradient(Q, grid)) / vp
-    N += dNdt * dt
-    N = jnp.where(N < 0, 0, N)
-    N = jnp.where(N > rho_i * g * thickness, rho_i * g * thickness, N)
-    N.at[0].set(0.0)
+lsq = least_squares(
+    lambda psi: solve(psi, discharge, 1e-2 * jnp.ones(n_cells)), 
+    jnp.zeros(n_cells), 
+    method = 'trf',
+    jac_sparsity = jax.jacrev(solve)(jnp.zeros(n_cells), discharge, 1e-2 * jnp.ones(n_cells)),
+    verbose = 2
+)
 
-    psi = base_gradient + np.gradient(N, grid)
+def solve_N(N, psi):
+    return base_gradient + jnp.gradient(N) - psi
 
-    N_fit = jnp.polyfit(grid, N, 3)
-    N = N_fit[0] * grid**3 + N_fit[1] * grid**2 + N_fit[2] * grid + N_fit[3]
+lsq_N = least_squares(
+    lambda N: solve_N(N, lsq.x), 
+    jnp.zeros(n_cells), 
+    method = 'trf',
+    jac_sparsity = jax.jacrev(solve_N)(jnp.zeros(n_cells), lsq.x)
+)
 
-    psi_fit = jnp.polyfit(grid, psi, 3)
-    psi = psi_fit[0] * grid**3 + psi_fit[1] * grid**2 + psi_fit[2] * grid + psi_fit[3]
-
-    return S, psi, N
-
-# Initial conditions
-S = S0 * jnp.ones(n_cells)
-N = jnp.zeros(n_cells)
-psi = base_gradient + np.gradient(N, grid)
-
-# Plot initial conditions
-# plt.plot(grid, bed, label = 'Bed elevation')
-# plt.plot(grid, surface, label = 'Surface elevation')
-# plt.xlabel('Distance from terminus (m)')
-# plt.ylabel('Elevation (m)')
-# plt.legend()
-# plt.show()
-
-# Time stepping
-dt = 100.0
-n_steps = 1000
-
-plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.viridis(np.linspace(0, 1, 10)))
-
-for t in range(n_steps):
-    S, psi, N = run_one_step(dt, S, psi, N)
-
-    if t % 100 == 99:
-        print(f'Step {t} of {n_steps}')
-
-        overburden = rho_i * g * thickness
-        plt.plot(grid, psi)
-
-plt.xlabel('Distance from terminus (m)')
-plt.ylabel('Hydraulic gradient (Pa m$^{-1}$)')
+plt.plot(grid, lsq_N.x * 1e-6)
 plt.show()
