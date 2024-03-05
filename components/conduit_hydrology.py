d9b6b9aeb079025d98d61379b4c6fff9b14ab0d3
Ethan Pierce <ethan.pierce@colorado.edu>
2024-03-05 10:06:55 -0700

    Add a priori flow routing to conduit system

--- components/conduit_hydrology.py
+++ components/conduit_hydrology.py
@@ -4,10 +4,61 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import equinox as eqx
+import jaxopt
+
+from landlab.components import FlowAccumulator
 
 from utils import StaticGrid
+from components import ModelState
 
 class ConduitHydrology(eqx.Module):
     """Implement a model of subglacial hydrology through a series of conduits."""
 
-    grid: StaticGrid
+    state: ModelState
+    grid: StaticGrid = eqx.field(init = False)
+    
+    conduit_area: jnp.array = eqx.field(converter = jnp.asarray)
+    melt_flux: jnp.array = eqx.field(converter = jnp.asarray, init = False)
+    potential: jnp.array = eqx.field(converter = jnp.asarray, init = False)
+    geometric_gradient: jnp.array = eqx.field(converter = jnp.asarray, init = False)
+
+    flow_coeff: float = 4.05e-2
+    flow_exp: float = 5 / 4 
+
+    def __post_init__(self):
+        """Initialize remaining model fields."""
+        self.grid = self.state.grid
+
+        self.melt_flux = self.state.melt_rate * self.grid.cell_area_at_node / self.state.sec_per_a
+
+        self.potential = (
+            self.state.water_density * self.state.gravity * self.state.bedrock_elevation
+            + self.state.water_pressure
+        )
+
+        self.geometric_gradient = (
+            -self.state.ice_density * self.state.gravity * self.state.surface_slope
+            -(self.state.water_density - self.state.ice_density) * self.state.gravity 
+            * self.state.bedrock_slope
+        )
+
+    def route_flow(self, landlab_grid) -> jnp.array:
+        """Route discharge based on the hydraulic potential field."""
+        fa = FlowAccumulator(
+            landlab_grid, 
+            surface = self.potential,
+            runoff_rate = self.melt_flux,
+            flow_director = 'FlowDirectorMFD'
+        )
+
+        area, discharge = fa.accumulate_flow(update_depression_finder = False)
+
+        return discharge
+
+    
+    
+    
+
+
+
+
--- tests/test_conduit_hydrology.py
+++ tests/test_conduit_hydrology.py
@@ -1,37 +1,92 @@
 """Test the ConduitHydrology implementation."""
 
 import numpy as np
-from numpy.testing import assert_array_almost_equal, assert_array_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal, assert_almost_equal
 import jax
 import jax.numpy as jnp
 import pytest
 
 from landlab import TriangleModelGrid
 from utils import StaticGrid, freeze_grid
-from components import ConduitHydrology
+from utils.plotting import plot_triangle_mesh, plot_links
+from components import ConduitHydrology, ModelState
 
 @pytest.fixture
 def grid():
     """Create a simple unstructured grid."""
     g = TriangleModelGrid(
         (
-            [-11, 10, 10, -10],
-            [10, 10, -10, -10]
+            [-1000, 1000, 1000, -1000],
+            [1000, 1000, -1000, -1000]
         ),
-        triangle_opts = 'pqDevjza0.25q27',
-        sort = True
+        triangle_opts = 'pqDevjza1000q26',
+        sort = False
+    )
+    static = freeze_grid(g)
+
+    g.add_field(
+        'surface_elevation', 
+        (np.max(g.node_x) - g.node_x)**(1/3) * 50,
+        at = 'node'
+    )
+
+    rng = np.random.default_rng(135)
+    g.add_field(
+        'bedrock_elevation',
+        (np.max(g.node_x) - g.node_x) * 0.05 + (rng.random(g.number_of_nodes)),
+        at = 'node'
+    )
+
+    g.add_field(
+        'ice_thickness',
+        g.at_node['surface_elevation'] - g.at_node['bedrock_elevation'],
+        at = 'node'
+    )
+    
+    g.add_field(
+        'sliding_velocity',
+        g.map_mean_of_link_nodes_to_link(np.max(g.node_x) + g.node_x),
+        at = 'link'
+    )
+
+    g.add_field(
+        'water_pressure',
+        g.at_node['ice_thickness'] * 9.81 * 917 * 0.8,
+        at = 'node'
+    )
+    
+    g.add_field(
+        'geothermal_heat_flux',
+        np.full(g.number_of_nodes, 0.05 * 31556926),
+        at = 'node'
     )
 
     return g
 
 @pytest.fixture
-def conduits(grid):
-    """Create an instance of the ConduitHydrology model."""
-    return ConduitHydrology(
-        freeze_grid(grid)
+def state(grid):
+    """Instantiate the ModelState."""
+    return ModelState(
+        freeze_grid(grid),
+        grid.at_node['ice_thickness'],
+        grid.at_node['surface_elevation'],
+        grid.at_link['sliding_velocity'],
+        grid.at_node['geothermal_heat_flux'],
+        grid.at_node['water_pressure']
     )
 
-def test_init(conduits):
-    """Test the initialization of the ConduitHydrology model."""
-    assert isinstance(conduits, ConduitHydrology)
-    assert isinstance(conduits.grid, StaticGrid)
+@pytest.fixture
+def conduits(state):
+    """Create an instance of the ConduitHydrology model."""
+    return ConduitHydrology(
+        state,
+        np.full(state.grid.number_of_links, 1e-3),
+    )
+
+def test_flow_routing(grid, conduits):
+    """Test the flow director."""
+    discharge = conduits.route_flow(grid)
+    assert discharge.shape == (grid.number_of_nodes,)
+    assert_almost_equal(jnp.sum(discharge), 5382.64, decimal = 2)
+
+    plot_triangle_mesh(grid, discharge, title = 'Discharge (m$^3$ s$^{-1}$)')
