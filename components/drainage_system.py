Fix unit normal calculation

components/drainage_system.py
@@ -211,15 +211,30 @@ class SubglacialDrainageSystem(eqx.Module):
         normal_at_links = self.grid.get_normal_at_links()
 
         def discharge_dot_normal(link):
-            left_patch = jnp.dot(
-                discharge[self.grid.patches_at_link][link, 0],
-                normal_at_links[link, 0]
+            head_xy = jnp.asarray([
+                self.grid.node_x[self.grid.node_at_link_head[link]],
+                self.grid.node_y[self.grid.node_at_link_head[link]]
+            ])
+            first_patch_xy = self.grid.xy_of_patch[self.grid.patches_at_link[link, 0]]
+            second_patch_xy = self.grid.xy_of_patch[self.grid.patches_at_link[link, 1]]
+
+            first_patch = jnp.where(
+                jnp.dot(first_patch_xy - head_xy, normal_at_links[link, 0]) > 0,
+                jnp.dot(discharge[self.grid.patches_at_link[link, 0]], normal_at_links[link, 0]),
+                jnp.dot(discharge[self.grid.patches_at_link[link, 0]], normal_at_links[link, 1])
             )
-            right_patch = jnp.dot(
-                discharge[self.grid.patches_at_link][link, 1],
-                normal_at_links[link, 1]
+
+            second_patch = jax.lax.cond(
+                self.grid.patches_at_link[link, 1] != -1,
+                lambda: jnp.where(
+                    jnp.dot(second_patch_xy - head_xy, normal_at_links[link, 1]) > 0,
+                    jnp.dot(discharge[self.grid.patches_at_link[link, 1]], normal_at_links[link, 1]),
+                    jnp.dot(discharge[self.grid.patches_at_link[link, 1]], normal_at_links[link, 0])
+                ),
+                lambda: 0.0
             )
-            return jnp.nansum(jnp.asarray([left_patch, right_patch]))
+
+            return first_patch + second_patch
         
         return jax.vmap(discharge_dot_normal)(jnp.arange(self.grid.number_of_links))
 

examples/hydrology/glads_config.py
@@ -94,59 +94,65 @@ def model(state, grid):
     """Create an instance of the SubglacialDrainageSystem model."""
     return SubglacialDrainageSystem(
         state, 
-        grid, 
         grid.at_node['surface_melt_rate'],
         state.overburden_pressure * 0.2,
+        np.full(grid.number_of_nodes, 0.05),
         np.full(grid.number_of_links, 0.0),
-        np.full(grid.number_of_nodes, 0.05)
     )
 
-def test_tmp(grid, model):
+def test_channel_source(grid, model):
+    m = model.exchange_term(grid.node_y**2, model.sheet_thickness)
 
-    model = model.update(60.0)
+    plot_triangle_mesh(grid, grid.node_y**2, subplots_args={'figsize': (18, 4)})
 
-    import time
-    start = time.time()
-    model = model.update(60.0)
-    print('Iteration time:', time.time() - start)
+    plot_links(grid, m, subplots_args={'figsize': (18, 4)})
 
-    for i in range(5):
-        model = model.update(60.0 * 60.0)
+# def test_tmp(grid, model):
 
-        print('Iteration', i)
+#     model = model.update(60.0)
 
-    plot_triangle_mesh(
-        grid,
-        model.base_potential - model.potential,
-        subplots_args = {'figsize': (18, 4)},
-        title = 'Effective pressure (Pa)'
-    )
+#     import time
+#     start = time.time()
+#     model = model.update(60.0)
+#     print('Iteration time:', time.time() - start)
 
-    Qc = jnp.abs(
-        -model.channel_conductivity
-        * model.channel_size**model.flow_exp
-        * model.grid.calc_grad_at_link(model.potential)
-    )
-    plot_links(
-        grid,
-        Qc,
-        subplots_args = {'figsize': (18, 4)},
-        title = 'Channelized discharge (m$^3$ s$^{-1}$)'
-    )
+#     for i in range(5):
+#         model = model.update(60.0 * 60.0)
 
-    gradient = model.grid.map_mean_of_links_to_node(
-        model.grid.calc_grad_at_link(model.potential)
-    )
-    Qs = jnp.abs(
-        -model.sheet_conductivity
-        * model.sheet_thickness**model.flow_exp
-        * jnp.power(jnp.abs(gradient), -1/2)
-        * gradient
-    )
-    plot_triangle_mesh(
-        grid,
-        Qs,
-        subplots_args = {'figsize': (18, 4)},
-        title = 'Distributed discharge (m$^3$ s$^{-1}$)'
-    )
+#         print('Iteration', i)
+
+#     plot_triangle_mesh(
+#         grid,
+#         model.base_potential - model.potential,
+#         subplots_args = {'figsize': (18, 4)},
+#         title = 'Effective pressure (Pa)'
+#     )
+
+#     Qc = jnp.abs(
+#         -model.channel_conductivity
+#         * model.channel_size**model.flow_exp
+#         * model.grid.calc_grad_at_link(model.potential)
+#     )
+#     plot_links(
+#         grid,
+#         Qc,
+#         subplots_args = {'figsize': (18, 4)},
+#         title = 'Channelized discharge (m$^3$ s$^{-1}$)'
+#     )
+
+#     gradient = model.grid.map_mean_of_links_to_node(
+#         model.grid.calc_grad_at_link(model.potential)
+#     )
+#     Qs = jnp.abs(
+#         -model.sheet_conductivity
+#         * model.sheet_thickness**model.flow_exp
+#         * jnp.power(jnp.abs(gradient), -1/2)
+#         * gradient
+#     )
+#     plot_triangle_mesh(
+#         grid,
+#         Qs,
+#         subplots_args = {'figsize': (18, 4)},
+#         title = 'Distributed discharge (m$^3$ s$^{-1}$)'
+#     )
 

utils/static_grid.py
@@ -45,6 +45,7 @@ class StaticGrid(eqx.Module):
     face_at_link: jax.Array = eqx.field(converter = jnp.asarray)
     link_at_face: jax.Array = eqx.field(converter = jnp.asarray)
     cell_at_node: jax.Array = eqx.field(converter = jnp.asarray)
+    node_at_cell: jax.Array = eqx.field(converter = jnp.asarray)
     corners_at_face: jax.Array = eqx.field(converter = jnp.asarray)
     nodes_at_patch: jax.Array = eqx.field(converter = jnp.asarray)
     patches_at_node: jax.Array = eqx.field(converter = jnp.asarray)
@@ -121,7 +122,7 @@ class StaticGrid(eqx.Module):
     def calc_grad_at_patch(self, array):
         """Calculate the gradient of an array at patches."""
         unit_normal = self.calc_unit_normal_at_patch(array)
-        theta = jnp.arctan2(-unit_normal[:, 1], -unit_normal[:, 0])
+        theta = jnp.arctan2(unit_normal[:, 1], unit_normal[:, 0])
         slope_at_patch = jnp.arccos(unit_normal[:, 2])
 
         components = (
@@ -188,27 +189,22 @@ class StaticGrid(eqx.Module):
 
     def get_normal_at_links(self):
         """Calculate the normal vector to each link."""
-        def at_one_link(l):
-            left = self.xy_of_patch[self.patches_at_link[l][0]]
-            right = jnp.where(
-                self.patches_at_link[l][1] != -1,
-                self.xy_of_patch[self.patches_at_link[l][1]],
-                jnp.asarray([jnp.nan, jnp.nan])
-            )
+        def norm_at_link(l):
+            head_x = self.node_x[self.node_at_link_head[l]]
+            head_y = self.node_y[self.node_at_link_head[l]]
 
-            mid = self.midpoint_of_link[l]
+            tail_x = self.node_x[self.node_at_link_tail[l]]
+            tail_y = self.node_y[self.node_at_link_tail[l]]
 
-            left_normal = jnp.asarray(
-                [left[0] - mid[0], left[1] - mid[1]]
-            )
+            dy = tail_y - head_y
+            dx = tail_x - head_x
 
-            right_normal = jnp.asarray(
-                [right[0] - mid[0], right[1] - mid[1]]
-            )
+            normal_a = jnp.asarray([-dy, dx])
+            normal_b = jnp.asarray([dy, -dx])
             
-            return jnp.asarray([left_normal, right_normal])
+            return jnp.asarray([normal_a, normal_b])
             
-        normals = jax.vmap(at_one_link)(jnp.arange(self.number_of_links))
+        normals = jax.vmap(norm_at_link)(jnp.arange(self.number_of_links))
         return normals
 
     def build_link_between_nodes_array(self):